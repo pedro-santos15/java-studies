f3ca5f1968ad6b5645c1aabfb00e583582974882
Pedro Santos <pedrohssantos1509@gmail.com>
2025-06-06 18:23:41 -0300

    refactor: aplica tratamento de exceções na calculadora

--- 0306/src/application/Main.java
+++ 0306/src/application/Main.java
@@ -2,12 +2,30 @@ package application;
 
 import entities.Calculadora;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Calculadora.calcular(5, 2, '+');
+        //exemplo de utilização
+        Calculadora.calcular(8, 2, '+');
         Calculadora.calcular(6, 3, '-');
         Calculadora.calcular(8, 2, '*');
         Calculadora.calcular(10, 2, '/');
-        Calculadora.calcular(10, 0, '/');
+        Calculadora.calcular(10, 0, '/'); //dando erro propositalmente
+
+        //verificacao de entrada
+        int entrada = 0;
+        while (entrada == 0){
+            try {
+                Scanner sc = new Scanner(System.in);
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                Calculadora.calcular(a, b, '+');
+                entrada = 1;
+            } catch (InputMismatchException e){
+                System.out.println("Favor informar uma entrada válida!");
+            }
+        }
     }
 }
--- 0306/src/entities/Calculadora.java
+++ 0306/src/entities/Calculadora.java
@@ -1,5 +1,6 @@
 package entities;
 
+
 public class Calculadora {
 
     public static double soma(double a, double b) {
@@ -24,19 +25,18 @@ public class Calculadora {
 
     public static void calcular(double a, double b, char operacao) {
         switch (operacao) {
-            case '+' -> System.out.println(soma(a, b));
-            case '-' -> System.out.println(subtrai(a, b));
-            case '*' -> System.out.println(multiplica(a, b));
+            case '+' -> System.out.printf("%.2f%n", soma(a, b));
+            case '-' -> System.out.printf("%.2f%n", subtrai(a, b));
+            case '*' -> System.out.printf("%.2f%n", multiplica(a, b));
             case '/' -> {
                 try {
-                    System.out.println(divisao(a, b));
+                    System.out.printf("%.2f%n", divisao(a, b));
                 } catch (IllegalArgumentException e) {
                     System.out.println(e.getMessage());
-
-
                 }
             }
             default -> System.out.println("Operação invalida!");
         }
+
     }
 }
